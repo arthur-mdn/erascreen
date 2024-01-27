b593d221458f873bb1fae8e6ef311025ff991b71
Arthur <arthur@mondon.pro>
2024-01-27 17:52:01 +0100

    conditionnaly display sections

--- client/src/components/Screen.jsx
+++ client/src/components/Screen.jsx
@@ -7,29 +7,49 @@ import PhotoSlider from "./PhotoSlider.jsx";
 import {Helmet} from "react-helmet";
 
 function Screen({configData}){
-
+    console.log(configData)
     return (
         <div>
             <Helmet>
                 <title>{configData.nom}</title>
             </Helmet>
             <div className={"fr jc-sb ai-c"}>
-                <img src={`${config.serverUrl}/${configData.logo}`} className={"card"} alt="Logo" style={{height:'6vw'}}/>
-                <MeteoViewer screen={configData}/>
+                {
+                    configData.logo && (
+                        <img src={`${config.serverUrl}/${configData.logo}`} className={"card"} alt="Logo" style={{height:'6vw'}}/>
+                    )
+                }
+                {
+                    configData.meteo && (
+                        <MeteoViewer screen={configData}/>
+                    )
+                }
                 <TimeViewer/>
-                <div className={"card fr ai-c g0-5"}>
-                    {configData.icons.map(icon => (
-                        <img key={icon} src={`${config.serverUrl}/${icon}`} alt={`Icon`} style={{width:'4vw', height:'4vw', objectFit:"contain"}} />
-                    ))}
-                </div>
+                {
+                    configData.icons && configData.icons.length > 0 && (
+                        <div className={"card fr ai-c g0-5"}>
+                            {configData.icons.map(icon => (
+                                <img key={icon} src={`${config.serverUrl}/${icon}`} alt={`Icon`} style={{width:'4vw', height:'4vw', objectFit:"contain"}} />
+                            ))}
+                        </div>
+                    )
+                }
             </div>
             <div style={{marginTop:'2vw', maxWidth:'100%', height:'75vh',maxHeight:'75vh', gap:'2vw'}} className={"fr jc-sb"}>
-                <DirectionsViewer screen={configData}/>
-                <PhotoSlider
-                    photos={configData.photos}
-                    interval={configData.config.photos_interval || 3000}
-                    hideDots={configData.config.hide_slider_dots}
-                />
+                {
+                    configData.directions && configData.directions.length > 0 && (
+                        <DirectionsViewer screen={configData}/>
+                    )
+                }
+                {
+                    configData.photos && configData.photos.length > 0 && (
+                        <PhotoSlider
+                            photos={configData.photos}
+                            interval={configData.config.photos_interval || 3000}
+                            hideDots={configData.config.hide_slider_dots}
+                        />
+                    )
+                }
             </div>
             {/*<h2 style={{fontSize:'1.3vw'}}>{configData.nom}</h2>*/}
             {/*<p>{configData._id}</p>*/}
