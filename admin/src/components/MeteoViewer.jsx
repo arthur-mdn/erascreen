f58507f37f3cb0ba5b8b3a2cc371a25278c5e914
Arthur <arthur@mondon.pro>
2024-01-29 13:51:04 +0100

    update admin MeteoView and prevent topbar to shrink

--- admin/src/components/MeteoViewer.jsx
+++ admin/src/components/MeteoViewer.jsx
@@ -3,16 +3,17 @@ function MeteoViewer({ screen }) {
     return (
         <>
             { screen.meteo && (
-                <div className={"meteo card fr ai-c"}>
+                <>
+                    <div className={"meteo card fr ai-c"}>
                     {
                         (screen.meteo.data && screen.meteo.data.weather) && (
                             <>
                                 {screen.meteo.data.weather[0].icon === '01n' && (
-                                    <img src="/elements/meteo/01n-edited.png" alt="Météo" style={{width:'6vw', height:'5vw', objectFit:"cover"}}/>
+                                    <img src="/elements/meteo/01n-edited.png" alt="Météo" style={{width:'5rem', height:'5rem', objectFit:"cover"}}/>
                                 )
                                 }
                                 {screen.meteo.data.weather[0].icon !== '01n' && (
-                                    <img src={`https://openweathermap.org/img/wn/${screen.meteo.data.weather[0].icon}@2x.png`} alt="Météo" style={{width:'6vw', height:'5vw', objectFit:"cover"}}/>
+                                    <img src={`https://openweathermap.org/img/wn/${screen.meteo.data.weather[0].icon}@2x.png`} alt="Météo" style={{width:'5rem', height:'5rem', objectFit:"cover"}}/>
                                 )
                                 }
                             </>
@@ -22,15 +23,22 @@ function MeteoViewer({ screen }) {
                     {
                         screen.meteo.data && (
                             <div>
-                                <h3 style={{fontSize:'1.3vw', fontWeight:"bold", lineHeight:'1.3vw'}}>{screen.meteo.data.name}</h3>
-                                <p  style={{fontSize:'1.3vw', fontWeight:"bold", lineHeight:'1.3vw'}}>{screen.meteo.data.main.temp}°C</p>
-                                <p style={{textTransform:"capitalize",fontSize:'1.3vw', fontWeight:"bold", lineHeight:'1.3vw'}}>{screen.meteo.data.weather[0].description}</p>
+                                <h3 style={{fontSize:'1.3rem', fontWeight:"bold", lineHeight:'1.5rem'}}>{screen.meteo.data.name}</h3>
+                                <p  style={{fontSize:'1rem', fontWeight:"bold", lineHeight:'1rem'}}>{screen.meteo.data.main.temp}°C</p>
+                                <p style={{textTransform:"capitalize",fontSize:'1rem', fontWeight:"bold", lineHeight:'1rem'}}>{screen.meteo.data.weather[0].description}</p>
                             </div>
                         )
                     }
 
-
                 </div>
+                    {
+                        screen.meteo && (
+                            <div>
+                                <p>Dernière mise à jour : {screen.meteo.date}</p>
+                            </div>
+                        )
+                    }
+                </>
             )
             }
         </>
