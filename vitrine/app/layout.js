c4591180415e8d089310da7749d2d9c57c8e5989
Arthur Mondon <arthur@mondon.pro>
2024-05-23 20:14:44 +0200

    prevent mobile zoom

--- vitrine/app/layout.js
+++ vitrine/app/layout.js
@@ -1,18 +1,21 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import Head from 'next/head';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
-  title: "DisplayHub",
-  description: "DisplayHub, une solution de gestion d'affichage dynamique."
+    title: "DisplayHub",
+    description: "DisplayHub, une solution de gestion d'affichage dynamique."
 };
 
-export default function RootLayout({ children }) {
-  return (
-  <html lang="fr">
-      <body className={inter.className}>{children}</body>
-  </html>
-  );
+export default function RootLayout({children}) {
+    return (
+        <html lang="fr">
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </Head>
+        <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
