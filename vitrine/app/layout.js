a7cba36c8815a4671459fc80fc8a33b1b1138598
Arthur Mondon <arthur@mondon.pro>
2024-05-23 20:08:04 +0200

    update favicon

--- vitrine/app/layout.js
+++ vitrine/app/layout.js
@@ -1,17 +1,18 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "DisplayHub",
-  description: "DisplayHub, une solution de gestion d'affichage dynamique",
+  description: "DisplayHub, une solution de gestion d'affichage dynamique."
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
+  <html lang="fr">
       <body className={inter.className}>{children}</body>
-    </html>
+  </html>
   );
 }
