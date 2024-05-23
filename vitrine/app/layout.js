7b2d5ff8a43e64c466f14546216c89c294b44313
Arthur Mondon <arthur@mondon.pro>
2024-05-23 22:38:24 +0200

    fix meta viewport

--- vitrine/app/layout.js
+++ vitrine/app/layout.js
@@ -1,6 +1,6 @@
 import {Inter} from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
+import type, { Viewport } from 'next'
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -8,13 +8,15 @@ export const metadata = {
     title: "DisplayHub",
     description: "DisplayHub, une solution de gestion d'affichage dynamique."
 };
+export const viewport = {
+    initialScale: 1,
+    width: 'device-width',
+    maximumScale: 1
+}
 
 export default function RootLayout({children}) {
     return (
         <html lang="fr">
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </Head>
         <body className={inter.className}>{children}</body>
         </html>
     );
