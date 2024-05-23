266e7e0662b463b96cc3fdbb3b56e39c816e65a4
Arthur Mondon <arthur@mondon.pro>
2024-05-23 19:33:39 +0200

    update vitrine website

--- vitrine/app/page.js
+++ vitrine/app/page.js
@@ -1,95 +1,76 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import {useForm, ValidationError} from '@formspree/react';
+
+function ContactForm() {
+    const [state, handleSubmit] = useForm("xayrzbbb");
+    if (state.succeeded) {
+        return <>
+            <Image src={"/check.png"} alt="envoyé" width={100} height={100}/>
+            <p style={{color: "white"}}>Merci pour votre intérêt ! Nous vous recontacterons très vite.</p>
+        </>;
+    }
+    return (
+        <>
+            <header className={styles.ctaCard__header}>
+                <p className={styles.ctaCard__title}>Envie d'obtenir un accès en anticipé ?</p>
+                <p className={styles.ctaCard__description}>
+                    Saisissez votre adresse e-mail pour obtenir un accès en avant-première à notre plateforme.
+                </p>
+            </header>
+            <form onSubmit={handleSubmit} className={styles.ctaCard__inputBox}>
+                <input
+                    id="email"
+                    className={styles.ctaCard__input}
+                    type="email"
+                    placeholder="votre-email@exemple.com"
+                    name="email"
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+                <button type="submit" disabled={state.submitting} className={styles.ctaCard__button}>
+                    <Image src={"/send.svg"} alt="Envoyer" width={24} height={24}/>
+                </button>
+            </form>
+        </>
+    );
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className={styles.main}>
+            <Image src={"logo.svg"} alt={"logo"} width={200} height={0} style={{height: "auto", width: "100%", maxWidth: "300px"}}/>
+            <div className={styles.description}>
+                <p>
+                    La solution d'affichage dynamique idéale. Diffusez vos contenus sur vos écrans en toute simplicité.
+                </p>
+            </div>
+            <div className={styles.center}>
+                <section className={styles.ctaCard}>
+                    <div className={styles.ctaCard__container}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                        <ContactForm/>
+                    </div>
+                </section>
+            </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            <div className={styles.grid}>
+                <a
+                    href="https://github.com/arthur-mdn/DisplayHub"
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2>
+                        GitHub <span>-&gt;</span>
+                    </h2>
+                    <p>Visualisez le code source de DisplayHub.</p>
+                </a>
+            </div>
+        </main>
+    );
 }
