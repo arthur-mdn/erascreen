dcc39f21c4d51ff6b4bd8fdb742d20e6f3a0715c
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2024-09-15 20:53:32 +0200

    Cache logo on first load

--- client/src/components/DisplayLogo.jsx
+++ client/src/components/DisplayLogo.jsx
@@ -5,51 +5,51 @@ import config from "../config.js";
 export default function displayLogo({logo, isDarkModeActive}) {
     const [cachedLogo, setCachedLogo] = useState(null);
 
-    useEffect(() => {
-        const loadLogo = async () => {
-            if (logo) {
-                const cachedLogoData = await getCachedLogo(logo._id);
-                if (cachedLogoData) {
-                    if (cachedLogoData.type === "image/svg+xml" && isDarkModeActive) {
-                        const reader = new FileReader();
-                        reader.onload = function(event) {
-                            let svgContent = event.target.result;
+    const loadLogo = async () => {
+        const cachedLogoData = await getCachedLogo(logo._id);
+        if (cachedLogoData) {
+            if (cachedLogoData.type === "image/svg+xml" && isDarkModeActive) {
+                const reader = new FileReader();
+                reader.onload = function (event) {
+                    let svgContent = event.target.result;
 
-                            const svgWithStyle = svgContent.replace(
-                                /<svg([^>]+)>/,
-                                `<svg$1><style>.fill-white-when-dark-mode{fill:#fff}</style>`
-                            );
+                    const svgWithStyle = svgContent.replace(
+                        /<svg([^>]+)>/,
+                        `<svg$1><style>.fill-white-when-dark-mode{fill:#fff}</style>`
+                    );
 
-                            const updatedBlob = new Blob([svgWithStyle], { type: 'image/svg+xml' });
-                            setCachedLogo(URL.createObjectURL(updatedBlob));
-                        };
-
-                        reader.readAsText(cachedLogoData);
-                    } else {
-                        setCachedLogo(URL.createObjectURL(cachedLogoData));
-                    }
-                } else {
-                    setCachedLogo(`${(logo.where === "server" ? config.serverUrl : "") + "/" + logo.value}`);
-                }
+                    const updatedBlob = new Blob([svgWithStyle], {type: 'image/svg+xml'});
+                    setCachedLogo(URL.createObjectURL(updatedBlob));
+                };
+                reader.readAsText(cachedLogoData);
+            } else {
+                setCachedLogo(URL.createObjectURL(cachedLogoData));
             }
-        };
+        } else {
+            setCachedLogo(`${(logo.where === "server" ? config.serverUrl : "") + "/" + logo.value}`);
+        }
+    };
 
-        loadLogo();
-    }, [logo, isDarkModeActive]);
 
     useEffect(() => {
-        if (logo) {
-            cacheLogo(logo);
+        async function cacheAndLoadLogo() {
+            if (logo) {
+                await cacheLogo(logo);
+            }
+            loadLogo();
         }
+
+        cacheAndLoadLogo();
     }, [logo]);
 
-  return (
-    <>
-        {
-            (cachedLogo && logo) && (
-                <img src={cachedLogo} className={"card logo"} alt="Logo" />
-            )
-        }
-    </>
-  );
+
+    return (
+        <>
+            {
+                (cachedLogo && logo) && (
+                    <img src={cachedLogo} className={"card logo"} alt="Logo"/>
+                )
+            }
+        </>
+    );
 }
