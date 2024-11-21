c9ddcc8637dd95cbe2040522357ae3aff7155c49
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2024-11-21 12:22:13 +0100

    Fix server urls .env

--- server/others/config.js
+++ server/others/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     dbUri: process.env.DB_URI,
     port: process.env.VITE_SERVER_PORT,
-    clientUrl: process.env.VITE_CLIENT_URL,
-    adminUrl: process.env.VITE_ADMIN_URL,
+    clientUrl: process.env.VITE_SCHEME + '://' + process.env.VITE_CLIENT_URL,
+    adminUrl: process.env.VITE_SCHEME + '://' + process.env.VITE_ADMIN_URL,
     secretKey: process.env.SECRET_KEY || 'SECRET_KEY_FOR_JWT',
     openWeatherApiKey: process.env.OPEN_WEATHER_API_KEY
 };
