5dfebf85105fd70f2701a1bb6d287cb45ecf02aa
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2024-11-21 12:15:55 +0100

    Update use of urls and scheme

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    clientUrl: import.meta.env.VITE_CLIENT_URL,
-    serverUrl: import.meta.env.VITE_SERVER_URL,
-    adminUrl: import.meta.env.VITE_ADMIN_URL,
+    clientUrl: import.meta.env.VITE_SCHEME + '://' + import.meta.env.VITE_CLIENT_URL,
+    serverUrl: import.meta.env.VITE_SCHEME + '://' + import.meta.env.VITE_SERVER_URL,
+    adminUrl: import.meta.env.VITE_SCHEME + '://' + import.meta.env.VITE_ADMIN_URL,
     cookieDomain: import.meta.env.VITE_COOKIE_DOMAIN,
     adminPort: import.meta.env.VITE_ADMIN_PORT
 };
